acee1073e99b8f45f5cb14d1f73f6877665259ad
Folowisreal <Folowoisreal15@gmail.com>
2022-09-09 18:45:43 -0700

    gittzy

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like bulding a mutilingual puzzle
-* Return: 0 
+* main - print the string in the put function 
+* Description: using the main function  
+*  this program prints "Programming is like building a multilingual puzzle  
+*  Return: 0  
 */
 int main(void)
 {
-     printf("with proper grammer, but the outcome is a piece of art, \n");
-     return (0);
+      print("with proper grammar, but the outcome is a piece of art, \n");
+      return (0);
 }
