d25293c544c9b495deb07468821c41e6599b6574
Folowisreal <Folowoisreal15@gmail.com>
2022-09-09 17:59:33 -0700

    gitty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
 /**
-* main  - print the string in the put function
+* main - print the stringg in the put function
 *
-*Description: using the main function
-*this program prints "Programming is like bulding a mutilingual puzzle
-*Return: 0
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return : 0
 */
 int main(void)
 {
-       puts("\"programming is like bulding a mutilingual puzzle");
-
-
-
-
+      puts("\"Programming is like building a multilingual puzzle");
       return (0);
 }
