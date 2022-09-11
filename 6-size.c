34779b5091ec5cbb09bada361b25d3e1ddc5f385
Folowisreal <Folowoisreal15@gmail.com>
2022-09-10 17:12:34 -0700

    git

--- 6-size.c
+++ 6-size.c
@@ -17,27 +17,31 @@
 */
 
 int main(void)
+
 {
-char c;
-int i;
 
-long li;
+	char c;
 
-long long lli;
+	int i;
 
-float f;
+	long li;
+
+	long long lli;
+
+	float f;
 
 
-printf("Size of a char: %ld byte(s)\n", sizeof(c));
 
-printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 
-printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 
-printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 
-printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 
-return (0);
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+	return (0);
 
 }
