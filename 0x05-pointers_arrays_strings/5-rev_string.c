96098d2694ef9bf27c23beb5902baf3d2c81fc34
Folowisreal <Folowoisreal15@gmail.com>
2022-09-19 17:34:13 -0700

    lila

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s = 10;
+
+	printf("%c\n", s)
+		rev_string(s);
+	printf("%c\n", s)
+		 return (0);
+}
